c1eccb7cc6c5577880d73802abdaa75ec855718f
palladino.11 <palladino.11@teleperformance.com.br>
2025-07-03 13:26:00 -0300

    Hide console window and prevent multiple instances
    - Added using directives for diagnostics and interop services.
    - Declared `FreeConsole()` method to hide the console window.
    - Implemented a check in `Main` to prevent multiple application instances.
    - Called `FreeConsole()` to hide the console at startup.
    - Added comments for clarity on the new functionality.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TPPreenchedor.Forms;
@@ -9,12 +11,22 @@ namespace TPPreenchedor
 {
     static class Program
     {
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool FreeConsole();
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            //VALIDA SE JÁ EXISTE INSTANCIA DA APLICAÇÃO
+            if (Process.GetProcessesByName(System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly()?.Location)).Count() > 1) return;
+
+            FreeConsole(); // Console desaparece COMPLETAMENTE
+            //FreeConsole(); // Console desaparece, mas ainda é possível abrir o console com F12 no Visual Studio
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Preenchedor());
